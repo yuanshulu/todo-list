763f258a282fd320d955712c496330266050c1eb
呂沅澍 <zero5405@gmail.com>
2021-08-04 22:28:26 +0800

    add ghpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  task = new FormControl('');
+  taskList = [
+    {title: 'Hit the gym', checked: true},
+    {title: 'Pay bills', checked: false},
+    {title: 'Meet George', checked: false},
+    {title: 'Buy eggs', checked: false},
+    {title: 'Read a book', checked: false},
+    {title: 'Organize office', checked: false},
+  ]
+  newTask() {
+    const title = this.task.value
+    if(!title) {
+      alert('you must write something!')
+      return
+    }
+    this.taskList.push({
+      title: title,
+      checked: false
+    })
+    this.task.setValue('')
+  }
+  checkElement(index:number) {
+    this.taskList[index].checked = !this.taskList[index].checked
+  }
+  deleteElement(index:number) {
+    this.taskList.splice(index, 1)
+  }
 }
